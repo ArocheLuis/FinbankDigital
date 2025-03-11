9f3b8b0a40072d073b27454f0a37c92762c237b2
ArocheLuis <larochem@miumg.edu.gt>
2025-03-10 21:35:28 -0600

    eliominar

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 mongoose.connect(config.mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
 })
 .then(() => console.log("Conectado a MongoDB Atlas"))
 .catch((error) => console.error("Error al conectar a MongoDB:", error));
